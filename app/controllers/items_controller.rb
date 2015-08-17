f277820580f1d3652b47e29f61deb5c8ef2c57cc
katiego <katiegaudin@gmail.com>
2015-08-17 16:19:15 -0700

    added items routes

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,20 @@
 class ItemsController < ApplicationController
-  def new
-  end
 
   def create
+    @user = current_user
+    @itinerary = Itinerary.find(params[:itinerary_id])
+    @item = @itinerary.items.create(item_params)
+    
+    if @item.save
+      redirect_to :back
+    else 
+      redirect_to :back
+    end
+  end
+
+  def show
+    @item = Item.find(params[:itinerary_id])
+    render :show
   end
 
   def destroy
@@ -13,4 +25,10 @@ class ItemsController < ApplicationController
 
   def update
   end
+
+  private
+      def item_params
+          params.require(:item).permit(:activity_name, :description, :category, :location)
+      end
 end
+
