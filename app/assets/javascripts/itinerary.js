442f7627d12aeacc8cca83654354284a1b8d0955
Manu Lohiya <manulohiya@gmail.com>
2015-08-19 13:57:00 -0700

    updated form fields

--- app/assets/javascripts/itinerary.js
+++ app/assets/javascripts/itinerary.js
@@ -8,7 +8,7 @@ function initialize() {
        var autocomplete = new google.maps.places.Autocomplete(input);
        google.maps.event.addListener(autocomplete, 'place_changed', function () {
            var place = autocomplete.getPlace();
-           document.getElementById('city2').value = place.name;
+           // document.getElementById('city2').value = place.name;
            document.getElementById('cityLat').value = place.geometry.location.lat();
            document.getElementById('cityLng').value = place.geometry.location.lng();
            // alert("This function is working!");
