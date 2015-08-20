52831150d8f45dd4083cc167e0575ee74fea0a48
aileen santos <asantos3026@gmail.com>
2015-08-20 15:26:11 -0700

    started mailer validation

---
+++ app/mailers/user_mailer.rb
@@ -0,0 +1,9 @@
+class UserMailer < ApplicationMailer
+  default from: "teamgitaround4@gmail.com"
+
+  def welcome_email(user)
+    @user = user
+    @url = "http://www.google.com"
+    mail(to: @user.email, subject: "test welcome email")
+  end
+end
